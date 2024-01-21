74d881cf41a0ed554a118805611122d25e7d2051
abderraouf-metidji <115145685+abderraouf-metidji@users.noreply.github.com>
2024-01-21 14:26:29 +0100

    name change and deletion

--- dash-app/custom_utils.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def round_numeric_columns(df, precision=2):
-    numeric_columns = df.select_dtypes(include='number').columns
-    df[numeric_columns] = df[numeric_columns].round(precision)
-    return df
